88e229a5bccb485379a803d8a0528a13c9f46437
kukecoolz <kuzacoolz@gmail.com>
2026-01-04 21:02:28 +0200

    Fix login bug and update UI

---
+++ .open-next/server-functions/default/.next/server/app/_global-error/page.js
@@ -0,0 +1,10 @@
+var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/app/_global-error/page.js")
+R.c("server/chunks/ssr/[root-of-the-server]__f62d412e._.js")
+R.c("server/chunks/ssr/_bd9cb0db._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__274d6f9d._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__b66f4a72._.js")
+R.c("server/chunks/ssr/node_modules_next_dist_81959fb0._.js")
+R.c("server/chunks/ssr/src_app_5b2047f8._.js")
+R.c("server/chunks/ssr/_next-internal_server_app__global-error_page_actions_75761787.js")
+R.m(61837)
+module.exports=R.m(61837).exports
